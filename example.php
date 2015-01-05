8a4d5c4876038d14f3e53a0f61d3c92a78fbbd9a
Alexandr Burov <skif@xwave.ru>
2015-01-05 17:49:17 +0300

    Remove main category

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
 $loader = new \Phalcon\Loader();
 
 $loader->registerNamespaces(array(
-    'Phalcon' => __DIR__ . '../../Phalcon'
+    'Phalcon' => __DIR__ . '/Phalcon'
 ));
 
 $loader->register();
